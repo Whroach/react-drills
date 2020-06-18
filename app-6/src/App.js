e090fcea8f2ce983e9bb2314a1cc4804a77625a7
whroach259@gmail.com <whroach259@gmail.com>
2020-06-18 17:36:25 -0500

    React2

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todo from './Components/Todo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor(){
+    super()
 
-export default App;
+    this.state={
+      addTask: "",
+      storeTask: [] ,
+    }
+
+  };
+
+  handleAdd = (value) => {
+    this.setState({addTask: value})
+
+  }
+
+  handleStore =(value) => {
+    this.setState({storeTask: value})
+  }
+
+  render(){
+    return(
+        <div>
+          <input onChange ={e => this.handleAdd(e.target.value)}></input>
+          <button onClick ={e => this.handleStore(e.target.value)}>Add</button>
+          <ul>{}</ul>
+
+        </div>
+    )
+  };}
---
+++ app-6/src/Components/Todo.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+import { render } from '@testing-library/react'
+
+export default class ToDo extends Component{
+    constructor(){
+        super()
+        
+        this.state = {
+
+        }
+
+
+
+
+    };
+
+
+    render(){
+        return(null)
+    }
+
+}
+
+
