98bf1930413e9124cede2604e4b7ec106000ea6a
whroach259@gmail.com <whroach259@gmail.com>
2020-06-18 15:57:47 -0500

    Complete

---
+++ app-5/src/Components/Image.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+
+export default class Image extends Component {
+    constructor(props){
+        super(props)
+
+    }
+
+
+
+    render(){
+        return(
+            <img src="https://i.pinimg.com/originals/01/c0/1c/01c01c45cb997f16675c5a5825645ceb.jpg" alt="picture of a monkey"></img>
+        )
+    }
+
+
+
+
+
+
+
+}
